00ea0a3e1b27db3e01afa2f482094951f85ba212
Mumuney Abdulquadri <abdlquadri@googlemail.com>
2021-12-22 10:31:17 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GodotAudioPlayerSignalDemo
+
+Godot master branch emits `finish` signal inconsistenty for different audio files.
+
+Audios `001001.mp3`, `001001.ogg` does not emit `finish` while `002030.mp3` does emit.
+
+Godot 3.4 behaves correctly. Meaning all files emit `finish` signal.
---
+++ TestingAudioPlayerSignals/Node2D.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AudioStreamPlayer.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_AudioStreamPlayer_finished():
+	print("fini")
+
+
+func _on_AudioStreamPlayer_ready():
+	print("ready")
